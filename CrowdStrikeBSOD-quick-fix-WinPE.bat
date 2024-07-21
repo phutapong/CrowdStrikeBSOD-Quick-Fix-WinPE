37a4f59cc7ac2b6d6a562ccbb2531f1d3ac893b1
phutapong <phutapong@outlook.com>
2024-07-21 16:50:40 +0700

    Update CrowdStrikeBSOD-quick-fix-WinPE.bat

--- CrowdStrikeBSOD-quick-fix-WinPE.bat
+++ CrowdStrikeBSOD-quick-fix-WinPE.bat
@@ -13,13 +13,19 @@ echo.
 timeout /t 3 /nobreak >nul
 
 :: Find CrowdStrike directory across all drives
+set fileFound=0
 for %%d in (C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
     if exist %%d:\%windir%\System32\drivers\CrowdStrike (
         echo Found CrowdStrike directory on drive %%d:
         cd /d %%d:\%windir%\System32\drivers\CrowdStrike
         del /f /q C-00000291*.sys
+        set fileFound=1
     )
 )
 
-echo Done!
-shutdown /r /t 1
+if %fileFound%==0 (
+    echo File C-00000291*.sys not found on any drive.
+) else (
+    echo Done!
+    shutdown /r /t 1
+)
